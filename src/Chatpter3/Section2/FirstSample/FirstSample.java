f53b2f955d04b0532254228007e87fcefec85293
DingJiaxiong <1612102142@qq.com>
2024-08-04 15:55:13 +0800

    初始化仓库

---
+++ src/Chatpter3/Section2/FirstSample/FirstSample.java
@@ -0,0 +1,24 @@
+package Chatpter3.Section2.FirstSample;
+
+/**
+ * This is the first sample program in Core Java Chapter 3
+ *
+ * @author Gary Cornell
+ * @version 1.01 1997-03-22
+ */
+
+public class FirstSample {
+
+    public static void main(String[] args) {
+
+        // 单行注释
+
+        /*
+            多行注释
+            多行注释
+            多行注释
+         */
+
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
---
+++ src/Chatpter3/Section3/DataType.java
@@ -0,0 +1,11 @@
+package Chatpter3.Section3;
+
+public class DataType {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
